1fcc8790d5a846ff02fae55d1c4c3ba94e4a9209
pateskinasy <pateskinasy@users.noreply.github.com>
2019-08-13 16:20:40 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@
 import newQuoteActionCreator from "../redux/actions/actions.js";
 import getNextQuoteReducer from "../redux/reducers/reducers.js";
 import getTwitterUrl from "./js-modules/getTwitterUrl.js";
-import makeRequest from "./js-modules/makeRequest.js/index.js";
+import makeRequest from "./js-modules/makeRequest.js";
 
 // The UMD build makes Redux available as a window.Redux global variable
 const Redux = window.Redux;
