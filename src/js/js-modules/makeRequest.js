34683d71fb231b1a195cba45aaa9719088e95c87
pateskinasy <pateskinasy@users.noreply.github.com>
2019-08-13 16:38:29 +0100

    Update makeRequest.js

--- src/js/js-modules/makeRequest.js
+++ src/js/js-modules/makeRequest.js
@@ -1,11 +1,11 @@
 //asynchronous function that gets data from the API and populates the quotes array
-const quotes;
+let quotes;
 
 const makeRequest = async () => {
   const responseJSON = await fetch(
     "https://cdn.jsdelivr.net/gh/morsewall/jsondb@master/db.json"
   );
-  return quotes = await responseJSON.json();
+  return (quotes = await responseJSON.json());
 };
 
 export { makeRequest, quotes };
