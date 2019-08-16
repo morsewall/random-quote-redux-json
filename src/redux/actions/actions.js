ccfbb57afee814044347baa99fc58becdb278e2e
pateskinasy <pateskinasy@users.noreply.github.com>
2019-08-16 19:02:18 +0100

    Update actions.js

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -2,7 +2,7 @@ import { NEW_QUOTE } from "../constants/constants.js";
 import { REQUESTING_API_DATA } from "../constants/constants.js";
 import { RECEIVED_API_DATA } from "../constants/constants.js";
 import getRandomElementSelector from "../selectors/selectors.js";
-import { store } from "../../../js/index.js";
+import { store } from "../../js/index.js";
 
 // defining action creators related to the asynch function. Action creator is  a function that returns an action (object that contains information about an action-event that has occurred). The action creator gets called by `dispatch()`
 export const requestingApiData = () => {
