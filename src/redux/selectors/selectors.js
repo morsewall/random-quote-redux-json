d108225e5df87fd23b31ace2545331460c82faa5
pateskinasy <pateskinasy@users.noreply.github.com>
2019-10-29 14:09:58 +0000

    Update selectors.js

--- src/redux/selectors/selectors.js
+++ src/redux/selectors/selectors.js
@@ -1,6 +1,6 @@
 //defining a Redux selection, i.e. it takes Redux state as an argument and return some data from it
-const getRandomElementSelector = state => {
-  let array = state.quotes;
+const getRandomElementSelector = stateObject => {
+  let array = stateObject.quotes;
   //access random quote from array
   return array[Math.floor(Math.random() * array.length)];
 };
